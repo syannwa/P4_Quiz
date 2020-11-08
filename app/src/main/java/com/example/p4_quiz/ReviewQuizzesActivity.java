5b7191be3f50e21b2f57751fb309a0249cca075f
Molly Hutchinson <45463598+mollyhutchinson@users.noreply.github.com>
2020-11-08 11:17:34 -0500

    comment fix

--- app/src/main/java/com/example/p4_quiz/ReviewQuizzesActivity.java
+++ app/src/main/java/com/example/p4_quiz/ReviewQuizzesActivity.java
@@ -46,7 +46,7 @@ public class ReviewQuizzesActivity extends AppCompatActivity {
 
     }
 
-    // This is an AsyncTask class (it extends AsyncTask) to read the quizzes
+    // This is an AsyncTask class (it extends AsyncTask) to read quizzes
     private class QuizzesDBReaderTask extends AsyncTask<Void, Void, List<QuizObject>> {
 
         @Override
